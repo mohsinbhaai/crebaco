a9aaeb9173960b79ac3f342efab89bce07c09a80
mohsinbhaai <mohsinnbhaai@gmail.com>
2018-08-10 19:26:02 +0530

    Initial commit

--- js/contact.js
+++ js/contact.js
@@ -11,3 +11,25 @@ function change() {
         // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
     }
 }
+
+function updateBackground() {
+    var
+        hr = (new Date()).getHours();
+
+        hello = document.querySelector(".hello");
+        var indianTime = document.querySelector(".indian-time");
+    if (hr < 10) {
+        hello.innerText = "Have a good morning";
+        hello.style.color="blue";
+    } else if (hr < 19) {
+        hello.innerText ="Have a good day!";
+        hello.style.color="red";
+    } else {
+        hello.innerText = "Have a good night!";
+        hello.style.color="yellow";
+        indianTime.style.backgroundColor="grey";
+    }
+}
+
+setInterval(updateBackground, 1000 * 60);
+updateBackground();
